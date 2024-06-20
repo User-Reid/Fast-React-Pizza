8bd3209b4f51eab914cf9611af32fb1b568f6dd7
Reid Herrera <reidherrerabusiness@gmail.com>
2024-06-20 11:36:13 -0500

    added more tailwind styling

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Reid</div>;
+  return <div className="hidden text-sm font-semibold md:block">Reid</div>;
 }
 
 export default Username;
