a0a9e7a9c2617128307505ca95f554be8c2f9e41
Reid Herrera <reidherrerabusiness@gmail.com>
2024-06-21 19:43:32 -0500

    Changed the favicon and styled the create order page and cart page.

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
   const base =
-    "inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+    "inline-block px-4 py-3 text-sm font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
+    secondary:
+      "inline-block px-4 py-2.5 font-semibold tracking-wide uppercase transition-colors border-2 border-stone-300 duration-300 rounded-full text-stone-400 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed md:px-6 md:py-3.5 hover:text-stone-800 focus:text-stone-800 text-sm",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
   };
 
